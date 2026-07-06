e0752c1981783e0549cc1934bbec7802fc46bcd4
dhalfks <miran5093@gmail.com>
2026-07-06 10:20:27 +0900

    jpa-9

--- src/main/resources/static/js/boardComment.js
+++ src/main/resources/static/js/boardComment.js
@@ -1,1 +1,52 @@
 console.log("boardComment.js in");
+
+// cmtAddBtn 버튼을 클릭하면 입력한 댓글과 작성자, bno 값을 controller 전송
+document.getElementById('cmtAddBtn').addEventListener('click',()=>{
+    const cmtText = document.getElementById('cmtText');
+    const cmtWrtier = document.getElementById('cmtWriter');
+
+    if(cmtText == null || cmtText.value.trim() == ''){
+        alert('댓글 입력요망!');
+        cmtText.focus();
+        return false;
+    }
+    let cmtData = {
+        bno: bno,
+        writer: cmtWrtier.innerText,
+        content: cmtText.value
+    }
+    console.log(cmtData);
+    registerCommentToServer(cmtData).then(result => {
+        if(result == '1'){
+            alert("댓글 등록 성공!");
+            // 댓글 입력창을 비우고, 포커스 맞추기
+            cmtText.value='';
+            cmtText.focus();
+        }else{
+            alert("댓글 등록 실패!");
+            cmtText.focus();
+        }
+    });
+    // 댓글 리스트 호출
+
+})
+
+// 전송 async 데이터 보내기
+async function registerCommentToServer(cmtData){
+    try {
+        const url = "/comment/post";
+        const config = {
+            method:'post',
+            headers : {
+                'content-type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify(cmtData)
+        };
+
+        const response = await  fetch(url, config);
+        const result = await response.text();
+        return result;
+    }catch (e) {
+        console.log(e);
+    }
+}
